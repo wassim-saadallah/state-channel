68592c64752f9ea94256a7ab52b4f0e42072b0a9
wassim-saadallah <wassim.saadalla@gmail.com>
2018-04-18 10:44:38 +0100

    add state-channel.sol

---
+++ state-channel.sol
@@ -0,0 +1,69 @@
+pragma solidity ^0.4.0;
+
+contract StateChannel {
+    
+    enum State{
+        PENDING,
+        OPENED,
+        CLOSING,
+        CLOSED,
+        DISPUTING,
+        CHALLENGED
+    }
+    
+    
+
+    address public player1;
+    address public player2;
+    bytes32 public finalState;
+    address public a1;
+    address public a2;
+    State public state;
+    
+    
+    event channelOpened (address player, address otherPlayer);
+    event channelClosed (address player, address otherPlayer, bytes32 lastState);
+    event dispute (address player, address otherPlayer, bytes32 lastState);
+    event challenge (address player, address otherPlayer, bytes32 lastState);
+    
+    
+    
+    function openChannel() public returns(State){
+        player1 = msg.sender;
+        state = State.PENDING;
+        return state;
+    }
+
+    
+    
+    function joinChannel() public {
+        if(state != State.PENDING) revert();
+        if(msg.sender == player1) revert();
+        player2 = msg.sender;
+        state = State.OPENED;
+        emit channelOpened(player1, player2);
+    }
+    
+    function closeChannel(bytes32 latestStateHash, bytes32 _r1, bytes32 _s1, uint8 _v1, bytes32 _r2, bytes32 _s2, uint8 _v2) public returns (bytes32){
+        
+        if(msg.sender != player1 || msg.sender != player2){
+            revert();
+        }
+        if(state != State.OPENED){
+            revert();
+        }
+        bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+        bytes32 prefixedHash = keccak256(prefix, latestStateHash);
+        a1 = ecrecover(prefixedHash, _v1, _r1, _s1);
+        a2 = ecrecover(prefixedHash, _v2, _r2, _s2);
+        if(a1 != player1 || a2 != player2){
+            if(a1 != player2 || a2 != player1){
+                revert();
+            }
+        }
+        
+        finalState = latestStateHash;
+        emit channelClosed(player1, player2, finalState);
+        return finalState;
+    }
+}
